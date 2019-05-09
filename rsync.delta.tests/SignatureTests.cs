3f94246983760501d3d5f420f15590a90db67bef
zmj <zach.jeyakaran@gmail.com>
2019-05-08 19:19:12 -0700

    add b2 test

--- rsync.delta.tests/SignatureTests.cs
+++ rsync.delta.tests/SignatureTests.cs
@@ -10,17 +10,19 @@ namespace Rsync.Delta.Tests
     {
         [Theory]
         [InlineData("hello_hellooo_b1")]
+        [InlineData("hello_hellooo_b2")]
         public async Task Signature(string dir)
         {
             dir = Path.GetFullPath($"../../../data/{dir}");
             byte[] expected  = await File.ReadAllBytesAsync(Path.Combine(dir, "v1.sig"));
+            int blockSize = int.Parse(dir[^1..]);
 
             byte[] actual = new byte[expected.Length];
             PipeWriter writer = PipeWriter.Create(new MemoryStream(actual));
             using (var f = File.OpenRead(Path.Combine(dir, "v1.txt")))
             {
                 PipeReader reader = PipeReader.Create(f);
-                await new Signature().Generate(reader, writer);
+                await new Signature().Generate(reader, writer, blockSize);
             }
             Console.WriteLine($"Expected: {BitConverter.ToString(expected)}");
             Console.WriteLine($"Actual: {BitConverter.ToString(actual)}");
--- rsync.delta/Signature.cs
+++ rsync.delta/Signature.cs
@@ -8,12 +8,12 @@ namespace Rsync.Delta
 {
     public class Signature
     {
-        public async Task Generate(PipeReader reader, PipeWriter writer)
+        public async Task Generate(PipeReader reader, PipeWriter writer, int blockSize)
         {
             if (reader == null) { throw new ArgumentNullException(nameof(PipeReader)); }
             if (writer == null) { throw new ArgumentNullException(nameof(PipeWriter)); }
 
-            WriteFileHeader(writer);
+            WriteFileHeader(writer, blockSize);
             await writer.FlushAsync(); // how often to flush?
 
             while (true)
@@ -23,7 +23,7 @@ namespace Rsync.Delta
                 {
                     result = await reader.ReadAsync();
                 }
-                WriteBlocks(result.Buffer, writer);
+                WriteBlocks(result.Buffer, writer, blockSize);
                 await writer.FlushAsync();
                 reader.AdvanceTo(result.Buffer.End);
                 if (result.IsCompleted)
@@ -33,7 +33,7 @@ namespace Rsync.Delta
             }
         }
 
-        private void WriteFileHeader(PipeWriter writer)
+        private void WriteFileHeader(PipeWriter writer, int blockSize)
         {
             var buffer = writer.GetSpan(sizeHint: 12);
 
@@ -41,8 +41,7 @@ namespace Rsync.Delta
             BinaryPrimitives.WriteUInt32BigEndian(buffer, (uint)format);
             buffer = buffer.Slice(start: 4);
 
-            uint bytesPerBlock = 1;
-            BinaryPrimitives.WriteUInt32BigEndian(buffer, bytesPerBlock);
+            BinaryPrimitives.WriteUInt32BigEndian(buffer, (uint)blockSize);
             buffer = buffer.Slice(start: 4);
             
             uint bytesPerStrongSum = 32;
@@ -51,28 +50,28 @@ namespace Rsync.Delta
             writer.Advance(bytes: 12);
         }
 
-        private void WriteBlocks(ReadOnlySequence<byte> buffer, PipeWriter writer)
+        private void WriteBlocks(ReadOnlySequence<byte> buffer, PipeWriter writer, int blockSize)
         {
             while (buffer.Length > 0)
             {
                 var span = buffer.First.Span;
-                for (int i=0; i<span.Length; i++)
+                for (int i=0; i<span.Length; i+= blockSize)
                 {
-                    WriteBlock(span.Slice(i, 1), writer);
+                    int end = (i + blockSize > span.Length) ? (span.Length-i) : blockSize;
+                    WriteBlock(span.Slice(i, end), writer);
                 }
+                // this doesn't consume entire span
                 buffer = buffer.Slice(span.Length);
             }
         }
 
         private void WriteBlock(ReadOnlySpan<byte> block, PipeWriter writer)
         {
-            Console.WriteLine($"b: {BitConverter.ToString(block.ToArray())}");
             var hash = new RollingHash();
             hash.Hash(block);
 
             var buffer = writer.GetSpan(sizeHint: 4);
             hash.WriteTo(buffer);
-            Console.WriteLine($"h: {BitConverter.ToString(buffer.Slice(0, 4).ToArray())}");
             writer.Advance(bytes: 4);
 
             buffer = writer.GetSpan(sizeHint: 32).Slice(0, 32);
