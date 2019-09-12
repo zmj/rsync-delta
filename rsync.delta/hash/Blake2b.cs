1289f1aead60cf0778d41f1c9564a0fa70be06e2
zmj <zach.jeyakaran@gmail.com>
2019-09-12 17:25:25 -0600

    starting to refactor blake2b

--- rsync.delta/hash/Blake2b.cs
+++ rsync.delta/hash/Blake2b.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Rsync.Delta.Hash
 {
+	/* Partial implementation of the blake2 hash algorithm.
+	 * Based on the reference implementation at https://github.com/BLAKE2/BLAKE2
+	 * Special thanks to Christian Winnerlein and Dominik Reichl.
+	 */
 	internal readonly struct Blake2b : IDisposable
 	{
 		private readonly IMemoryOwner<byte> _scratch;
@@ -34,10 +39,7 @@ namespace Rsync.Delta.Hash
 			core.HashFinal(hash, isEndOfLayer: false);
 		}
 
-		public void Dispose()
-		{
-			_scratch.Dispose();
-		}
+		public void Dispose() => _scratch.Dispose();
 	}
 
     internal ref struct Blake2bCore
@@ -54,19 +56,19 @@ namespace Rsync.Delta.Hash
 		private ulong _finalizationFlag0;
 		private ulong _finalizationFlag1;
 
-		private const int NumberOfRounds = 12;
-		private const int BlockSizeInBytes = 128;
+		private const int _numRounds = 12;
+		private const int _blockSize = 128;
 
-		const ulong IV0 = 0x6A09E667F3BCC908UL;
-		const ulong IV1 = 0xBB67AE8584CAA73BUL;
-		const ulong IV2 = 0x3C6EF372FE94F82BUL;
-		const ulong IV3 = 0xA54FF53A5F1D36F1UL;
-		const ulong IV4 = 0x510E527FADE682D1UL;
-		const ulong IV5 = 0x9B05688C2B3E6C1FUL;
-		const ulong IV6 = 0x1F83D9ABFB41BD6BUL;
-		const ulong IV7 = 0x5BE0CD19137E2179UL;
+		private const ulong IV0 = 0x6A09E667F3BCC908UL;
+		private const ulong IV1 = 0xBB67AE8584CAA73BUL;
+		private const ulong IV2 = 0x3C6EF372FE94F82BUL;
+		private const ulong IV3 = 0xA54FF53A5F1D36F1UL;
+		private const ulong IV4 = 0x510E527FADE682D1UL;
+		private const ulong IV5 = 0x9B05688C2B3E6C1FUL;
+		private const ulong IV6 = 0x1F83D9ABFB41BD6BUL;
+		private const ulong IV7 = 0x5BE0CD19137E2179UL;
 
-		private static readonly int[] Sigma = new int[NumberOfRounds * 16] 
+		private static readonly int[] Sigma = new int[_numRounds * 16] 
 		{
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 			14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3,
@@ -91,7 +93,7 @@ namespace Rsync.Delta.Hash
 			_m = MemoryMarshal.Cast<byte, ulong>(scratch.Slice(256, 128));
 			_h = MemoryMarshal.Cast<byte, ulong>(scratch.Slice(384, 64));
 
-			_h[0] = IV0;
+			_h[0] = IV0 ^ 0x01_01_00_20;
 			_h[1] = IV1;
 			_h[2] = IV2;
 			_h[3] = IV3;
@@ -105,37 +107,6 @@ namespace Rsync.Delta.Hash
 			_finalizationFlag0 = 0;
 			_finalizationFlag1 = 0;
 			_bufferFilled = 0;
-
-			Span<ulong> config = stackalloc ulong[8];
-			const ulong treeIV = 0x01_01_00_20;
-			config[0] = treeIV;
-			for (int i = 0; i < 8; i++)
-				_h[i] ^= config[i];
-		}
-
-		internal static ulong BytesToUInt64(ReadOnlySpan<byte> buf, int offset)
-		{
-			return
-				((ulong)buf[offset + 7] << 7 * 8 |
-				((ulong)buf[offset + 6] << 6 * 8) |
-				((ulong)buf[offset + 5] << 5 * 8) |
-				((ulong)buf[offset + 4] << 4 * 8) |
-				((ulong)buf[offset + 3] << 3 * 8) |
-				((ulong)buf[offset + 2] << 2 * 8) |
-				((ulong)buf[offset + 1] << 1 * 8) |
-				((ulong)buf[offset]));
-		}
-
-		private static void UInt64ToBytes(ulong value, Span<byte> buf, int offset)
-		{
-			buf[offset + 7] = (byte)(value >> 7 * 8);
-			buf[offset + 6] = (byte)(value >> 6 * 8);
-			buf[offset + 5] = (byte)(value >> 5 * 8);
-			buf[offset + 4] = (byte)(value >> 4 * 8);
-			buf[offset + 3] = (byte)(value >> 3 * 8);
-			buf[offset + 2] = (byte)(value >> 2 * 8);
-			buf[offset + 1] = (byte)(value >> 1 * 8);
-			buf[offset] = (byte)value;
 		}
 
 		public void HashCore(ReadOnlySpan<byte> array)
@@ -143,13 +114,13 @@ namespace Rsync.Delta.Hash
 			int start = 0;
 			int count = array.Length;
 			int offset = start;
-			int bufferRemaining = BlockSizeInBytes - _bufferFilled;
+			int bufferRemaining = _blockSize - _bufferFilled;
 
-			if ((_bufferFilled > 0) && (count > bufferRemaining))
+			if (_bufferFilled > 0 && count > bufferRemaining)
 			{
 				array.Slice(offset, bufferRemaining).CopyTo(
 					_buf.Slice(_bufferFilled));
-				_counter0 += BlockSizeInBytes;
+				_counter0 += _blockSize;
 				if (_counter0 == 0)
 					_counter1++;
 				Compress(_buf, 0);
@@ -158,14 +129,14 @@ namespace Rsync.Delta.Hash
 				_bufferFilled = 0;
 			}
 
-			while (count > BlockSizeInBytes)
+			while (count > _blockSize)
 			{
-				_counter0 += BlockSizeInBytes;
+				_counter0 += _blockSize;;
 				if (_counter0 == 0)
 					_counter1++;
 				Compress(array, offset);
-				offset += BlockSizeInBytes;
-				count -= BlockSizeInBytes;
+				offset += _blockSize;
+				count -= _blockSize;
 			}
 
 			if (count > 0)
@@ -178,7 +149,6 @@ namespace Rsync.Delta.Hash
 
 		public void HashFinal(Span<byte> result, bool isEndOfLayer)
 		{
-			//Last compression
 			_counter0 += (uint)_bufferFilled;
 			_finalizationFlag0 = ulong.MaxValue;
 			if (isEndOfLayer)
@@ -187,14 +157,13 @@ namespace Rsync.Delta.Hash
 				_buf[i] = 0;
 			Compress(_buf, 0);
 
-			//Output
 			Span<byte> hash = stackalloc byte[64];
 			for (int i = 0; i < 8; ++i)
 			{
-				UInt64ToBytes(_h[i], hash, i << 3);
+				var buf = hash.Slice(i << 3);
+				BinaryPrimitives.WriteUInt64LittleEndian(buf, _h[i]);
 			}
 
-			//Truncate
             if (result.Length < hash.Length)
 			{
 				hash = hash.Slice(0, result.Length);
@@ -210,49 +179,33 @@ namespace Rsync.Delta.Hash
 		private void G(int a, int b, int c, int d, int r, int i)
 		{
 			int p = (r << 4) + i;
-			int p0 = Sigma[p];
-			int p1 = Sigma[p + 1];
-			var v = _v;
-			var m = _m;
-
-			v[a] += v[b] + m[p0];
-			v[d] = RotateRight(v[d] ^ v[a], 32);
-			v[c] += v[d];
-			v[b] = RotateRight(v[b] ^ v[c], 24);
-			v[a] += v[b] + m[p1];
-			v[d] = RotateRight(v[d] ^ v[a], 16);
-			v[c] += v[d];
-			v[b] = RotateRight(v[b] ^ v[c], 63);
+			_v[a] += _v[b] + _m[Sigma[p]];
+			_v[d] = RotateRight(_v[d] ^ _v[a], 32);
+			_v[c] += _v[d];
+			_v[b] = RotateRight(_v[b] ^ _v[c], 24);
+			_v[a] += _v[b] + _m[Sigma[p + 1]];
+			_v[d] = RotateRight(_v[d] ^ _v[a], 16);
+			_v[c] += _v[d];
+			_v[b] = RotateRight(_v[b] ^ _v[c], 63);
 		}
 
 		private void Compress(ReadOnlySpan<byte> block, int start)
 		{
-			var v = _v;
-			var h = _h;
-			var m = _m;
+			MemoryMarshal.Cast<byte, ulong>(block)
+				.Slice(0, 16)
+				.CopyTo(_m);
 
-			for (int i = 0; i < 16; ++i)
-				m[i] = BytesToUInt64(block, start + (i << 3));
+			_h.CopyTo(_v);
+			_v[8] = IV0;
+			_v[9] = IV1;
+			_v[10] = IV2;
+			_v[11] = IV3;
+			_v[12] = IV4 ^ _counter0;
+			_v[13] = IV5 ^ _counter1;
+			_v[14] = IV6 ^ _finalizationFlag0;
+			_v[15] = IV7 ^ _finalizationFlag1;
 
-			v[0] = h[0];
-			v[1] = h[1];
-			v[2] = h[2];
-			v[3] = h[3];
-			v[4] = h[4];
-			v[5] = h[5];
-			v[6] = h[6];
-			v[7] = h[7];
-
-			v[8] = IV0;
-			v[9] = IV1;
-			v[10] = IV2;
-			v[11] = IV3;
-			v[12] = IV4 ^ _counter0;
-			v[13] = IV5 ^ _counter1;
-			v[14] = IV6 ^ _finalizationFlag0;
-			v[15] = IV7 ^ _finalizationFlag1;
-
-			for (int r = 0; r < NumberOfRounds; ++r)
+			for (int r = 0; r < _numRounds; ++r)
 			{
 				G(0, 4, 8, 12, r, 0);
 				G(1, 5, 9, 13, r, 2);
@@ -266,7 +219,7 @@ namespace Rsync.Delta.Hash
 
 			for (int i = 0; i < 8; ++i)
 			{
-				h[i] ^= v[i] ^ v[i + 8];
+				_h[i] ^= _v[i] ^ _v[i + 8];
 			}
 		}
     }
