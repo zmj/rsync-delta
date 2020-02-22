ab5d5c74fd661489272a246372a67b6aa46fae45
zmj <zach.jeyakaran@gmail.com>
2020-02-22 15:05:02 -0700

    part of slidingBlock internals

--- rsync.delta/delta/SlidingBlock.cs
+++ rsync.delta/delta/SlidingBlock.cs
@@ -5,13 +5,11 @@ namespace Rsync.Delta.Delta
 {
     internal ref struct SlidingBlock
     {
-        private ref struct Position
-        {
-            public ReadOnlySequence<byte>.Enumerator MemoryEnumerator;
-            public long SpanPositionInSequence;
-            public ReadOnlySpan<byte> Span;
-            public int PositionInSpan;
-        }
+        private readonly int _blockLength;
+        private readonly bool _isFinalBlock;
+
+        private Position _start;
+        private Position _end;
 
         public SlidingBlock(
             in ReadOnlySequence<byte> sequence,
@@ -21,14 +19,39 @@ namespace Rsync.Delta.Delta
             throw new NotImplementedException();
         }
 
+        private ref struct Position
+        {
+            private ReadOnlySequence<byte>.Enumerator _enumerator;
+            private long _seqPos;
+            private ReadOnlySpan<byte> _span;
+            private int _spanPos;
+
+            public bool TryAdvance(
+                out long sequencePosition,
+                out byte previous, 
+                out byte current)
+            {
+                // two-tier enumeration
+                // enumerate within current spans (inner loop)
+                // else advance to next span
+                throw new NotImplementedException();
+            }
+        }
+
         public bool TryAdvance(
             out long start, out long length, 
             out byte removed, out byte added)
         {
-            // two-tier enumeration
-            // enumerate within current spans (inner loop)
-            // else advance to next span
-            throw new NotImplementedException();
+            if ((_end.TryAdvance(out long end, out _, out added) || _isFinalBlock) &&
+                _start.TryAdvance(out start, out removed, out _))
+            {
+                length = end - start;
+                return true;
+            }
+            start = default;
+            length = default;
+            removed = default;
+            return false;
         }
     }
 }
